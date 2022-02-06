7147ee841d4c8bb6a39d9fddb882b0460c8e8f95
trunkey2007 <hoquanglam090@gmail.com>
2022-02-06 14:24:12 +0700

    fix log out button

--- src/components/UserModal.js
+++ src/components/UserModal.js
@@ -15,7 +15,7 @@ export default function UserModal(props) {
                     <img className="on-hover" alt="user-avatar" src={props.userIcon}></img>
                     {(props.userDetail?.onAccess !== props.userDetail?.username)? <Button onClick={() => {window.location = `/user/profile/${props.userDetail.onAccess}`}}>Home <i className="fas fa-home"></i></Button> : <></>}
                     <Button onClick={() => {window.location = `/user/profile/${props.userDetail.username}`}}>Profile <i className="fas fa-user-circle"></i></Button>
-                    <Button onClick={() => {signOut()}} >Log Out <i className="fas fa-sign-out-alt"></i></Button>
+                    {(props.userDetail?.onAccess !== props.userDetail?.username)? <Button onClick={() => {signOut()}} >Log Out <i className="fas fa-sign-out-alt"></i></Button> : <></>}
                 </div>
             </Modal.Body>
         </Modal>
